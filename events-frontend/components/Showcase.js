e59a7e508026c135e22dedb32672fddfe0eb7d79
arielle1502 <arielle1502@yahoo.com>
2021-05-06 10:35:48 +1000

    showcase and api routes

---
+++ events-frontend/components/Showcase.js
@@ -0,0 +1,14 @@
+import styles from '@/styles/Showcase.module.css'
+
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the Party</h1>
+      <h2>wakka wakka wakka</h2>
+      
+    </div>
+  )
+}
+
+
