7355334eee578de40c93ee1ad0e7241d7f1e6852
arielle1502 <arielle1502@yahoo.com>
2021-05-11 19:29:39 +1000

    created logout adn cookies

--- events-frontend/components/Header.js
+++ events-frontend/components/Header.js
@@ -2,8 +2,12 @@ import Link from 'next/link'
 import styles from '@/styles/Header.module.css'
 import Search from './Search'
 import {FaSignInAlt, FaSignOutAlt} from 'react-icons/fa'
+import AuthContext from '@/context/AuthContext'
+import {useContext} from 'react'
 
 export default function Header() {
+
+  const {user, logout} = useContext(AuthContext)
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -21,16 +25,35 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
+          {user ? 
+          //if logged in
+          <>
+            <li>
             <Link href='/events/add'>
             <a>Add Event</a>
             </Link>
           </li>
+          <li>
+            <Link href='/account/dashboard'>
+            <a>Dashboard</a>
+            </Link>
+          </li>
+          <li>
+            <button onClick={() => logout()} className="btn-secondary btn-icon">
+              <FaSignOutAlt/>Logout
+            </button>
+          </li>
+          </> : 
+          //iff logged out
+          <>
           <li> 
             <Link href='/account/login'>
             <a className='btn-secondary btn-icon'>
             <FaSignInAlt/>Login</a>
             </Link></li>
+          </>}
+         
+          
         </ul>
       </nav>
       
--- events-frontend/pages/account/login.js
+++ events-frontend/pages/account/login.js
@@ -5,14 +5,19 @@ import {useState, useEffect, useContext} from 'react'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
 import styles from '@/styles/AuthForm.module.css'
+import AuthContext from '@/context/AuthContext'
 
 export default function LoginPage() {
   const [email, setEmail] =useState('')
   const [password, setPassword] =useState('')
 
+  const {login, error} = useContext(AuthContext)
+
+  useEffect(() => error && toast.error(error))
+
   const handleSubmit= (e) => {
     e.preventDefault()
-    console.log({email, password})
+    login({email, password})
   }
 
   return <Layout title='User Login'>
